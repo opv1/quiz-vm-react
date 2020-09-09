6f931c679f184ff844a324921ef271cf07e506ac
opv1 <pas7273@gmail.com>
2020-09-09 17:56:20 +0300

    Updates

---
+++ precache-manifest.c2aa6bb53c49ae5534d4aeb89f90d678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f4cae9aa05ab5d16de8f81b90bb493",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "7e7c8f3c18d85b38f86a",
+    "url": "/quiz-app/static/css/main.a78d127e.chunk.css"
+  },
+  {
+    "revision": "729ad5721c51724bbd66",
+    "url": "/quiz-app/static/js/2.2df24f63.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/quiz-app/static/js/2.2df24f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7c8f3c18d85b38f86a",
+    "url": "/quiz-app/static/js/main.534c52d9.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
